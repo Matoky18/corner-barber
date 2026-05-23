b2941ee0a7e5bacb58c7cb031dd5397694456890
Matoky18 <matokyranay@gmail.com>
2026-05-23 13:25:53 +0300

    make contact section responsive

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -35,15 +35,15 @@ const page = () => {
   const service = urlParams.get('service')
 
 
-
    const flexCenter= ' flex justify-center items-center  '
    const inputStyle = `${flexCenter} p-3 border-1 border-[gray] rounded-[5px] mb-[12.5px] `
+   const buttonStyle = "cursor-pointer w-[100%] text-white bg-gradient-dark h-[50px] border-1 border-[gray] rounded-[5px] "
 
   return (
-    <div className= {`pt-[100px] min-h-[calc(100vh-150px)]  ${flexCenter} flex-col  `} >
+    <div className= {`pt-[100px] h-[100vh] flex  items-between ]  ${flexCenter} flex-col  `} >
         <h1 className='text-[30px]  tracking-wider' >Réservation</h1>
-        <p className='mb-[50px] w-[350px] text-center ' >Réservez votre coupe en quelques clics et choisissez l’horaire qui vous convient</p>
-        <form className= {`flex-col  flex w-[350px]  `} onSubmit={handleSubmit} >
+        <p className='mb-[50px]  max-w-[350px] w-[calc(100vw-50px)]  text-center ' >Réservez votre coupe en quelques clics et choisissez l’horaire qui vous convient</p>
+        <form className= {`flex-col pb-[64px] flex max-w-[350px] w-[calc(100vw-50px)]  `} onSubmit={handleSubmit} >
 
             <input className= {`${inputStyle} h-[50px] `} placeholder='Nom' type="text" id="name" value={form.name} name="name" onChange={handleChange} />
 
@@ -54,7 +54,6 @@ const page = () => {
 
             <div className= {`flex`} >
 
-                {/* <label htmlFor="service">Service</label> */}
                 <select defaultValue={`${service}` || ""  } className= {`cursor-pointer ${inputStyle} w-[100%] mb-[10px] h-[50px]`}  name="service" id="service">
 
                   <option  value="">Choisir un service</option>
@@ -67,10 +66,9 @@ const page = () => {
             </div>
             
 
-            {/* <label htmlFor="message">Message:</label> */}
             <textarea className= {`resize-none ${inputStyle} h-[150px] `} placeholder="Votre message..." id="message" name="message" value={form.message} onChange={handleChange}  ></textarea>
 
-            <button className= {`cursor-pointer w-[350px] text-white bg-gradient-dark h-[50px] border-1 border-[gray] rounded-[5px] `} type="submit">Confirmer le rendez-vous</button>
+            <button className= {`${buttonStyle}`} type="submit">Confirmer le rendez-vous</button>
         
         </form>
     </div>
